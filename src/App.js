708dcd7413c135c3e1fca53f6fed3dbc1beb9bd6
Tyrrnien81 <june121284@gmail.com>
2024-12-11 03:22:58 -0600

    fixed directory

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import React from "react";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-const App = () => {
-    return <div>App</div>;
-};
+import Header from "./Header/Header";
+import Home from "./Home/Home";
+import About from "./About/About";
+import Skills from "./Skills/Skills";
+import Projects from "./Projects/Projects";
+import Contact from "./Contact/Contact";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className="App">
+                {/* Header outside of Routes to display on all pages */}
+                <Header />
+                <Routes>
+                    {/* Default Home Page */}
+                    <Route
+                        path="/"
+                        element={
+                            <>
+                                <div id="home">
+                                    <Home />
+                                </div>
+                                <div id="about">
+                                    <About />
+                                </div>
+                                <div id="skills">
+                                    <Skills />
+                                </div>
+                                <div id="projects">
+                                    <Projects />
+                                </div>
+                                <div id="contact">
+                                    <Contact />
+                                </div>
+                            </>
+                        }
+                    />
+                    {/* Redirect unknown paths to Home Page */}
+                    <Route path="*" element={<Navigate to="/" />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
+}
 
 export default App;
